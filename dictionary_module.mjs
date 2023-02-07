5eebb7ad2dff8cd8f0e3e470db58ffe7bd9f348b
prebenrosland <preb1.rosland@gmail.com>
2023-02-07 14:49:24 +0100

    module

--- dictionary_module.mjs
+++ dictionary_module.mjs
@@ -1,35 +1,10 @@
-
-class Dictionary{
-
-    constructor(...languages){
-        this.languagefiles = languages
-        this.dictionary = null;
-        this.currentLanguage = null
-    }
-
-    setLanguage = async function(lang){
-        // Velger språket som skal brukes.
-
-        let languageFile = this.languagefiles.find( file => {
-            return file.indexOf(lang) != -1
-        });
-
-        this.currentLanguage = lang;
-
-        this.dictionary = await import(languageFile, { assert: { type: "json" } });
-    }
-
-    get = function(key){
-        // Henter riktig verdi basert på key i dictionary.
-        return this.dictionary[key];
-    }
-
-    keys = function(){
-        // retunerer alle keys i dictionary
-        return Object.keys( this.dictionary);
+const Dictionary = {
+    en:{
+        "btn": "english button"
+    },
+    no:{
+        "btn": "norsk knapp"
     }
 }
-
-let norsk = new Dictionary("./no.json");
 
 export default Dictionary;
---
+++ test/dictionaryTest.mjs
@@ -0,0 +1,8 @@
+import Dictionary from "../dictionary_module.mjs";
+
+
+if(Dictionary.en.btn === "english button"){
+    console.log("ok");
+} else{
+    console.log("errrr")
+}
