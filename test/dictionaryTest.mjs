3e7a8cf1accdbc43ec93795cc8dee79cd94ec791
prebenrosland <preb1.rosland@gmail.com>
2023-02-07 14:52:25 +0100

    a

--- test/dictionaryTest.mjs
+++ test/dictionaryTest.mjs
@@ -4,5 +4,5 @@ import Dictionary from "../dictionary_module.mjs";
 if(Dictionary.en.btn === "english button"){
     console.log("ok");
 } else{
-    console.log("errrr")
+    console.log("error");
 }
